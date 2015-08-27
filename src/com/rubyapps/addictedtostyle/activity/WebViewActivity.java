7dacc6f444b6c506665e5d3feef2bb77bdb1e202
Andriy Lendyel <flame27@meta.ua>
2015-08-28 00:37:30 +0300

    format code

--- src/com/rubyapps/addictedtostyle/activity/WebViewActivity.java
+++ src/com/rubyapps/addictedtostyle/activity/WebViewActivity.java
@@ -24,169 +24,170 @@ import android.widget.ArrayAdapter;
 
 public class WebViewActivity extends SherlockActivity {
 
-    private WebView webView;
-    private ActionBar actionBar;
-    // Refresh menu item
-    private MenuItem refreshMenuItem;
+	private WebView webView;
+	private ActionBar actionBar;
+	// Refresh menu item
+	private MenuItem refreshMenuItem;
 
-    private List<GridItem> itemsList;
-    private NavigationListener navigationListener;
-    private String url;
+	private List<GridItem> itemsList;
+	private NavigationListener navigationListener;
+	private String url;
 
-    protected void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.web_view_activity);
-	Appodeal.show(this, Appodeal.BANNER_BOTTOM);
-	url = getIntent().getStringExtra("url");
-	webView = (WebView) findViewById(R.id.webView);
-	webView.getSettings().setJavaScriptEnabled(true);
-	webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-	webView.getSettings().setDatabaseEnabled(true);
-	webView.getSettings().setDomStorageEnabled(true);
-	webView.getSettings().setBuiltInZoomControls(true);
-	webView.getSettings().setSupportZoom(true);
-	webView.getSettings().setLoadWithOverviewMode(true);
-	webView.getSettings().setUseWideViewPort(true);
-	webView.setWebViewClient(new WebViewClient() {
-	    @Override
-	    public void onPageStarted(WebView view, String url, Bitmap favicon) {
-		if (refreshMenuItem != null) {
-		    refreshMenuItem.setActionView(R.layout.action_progressbar);
-		    refreshMenuItem.expandActionView();
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.web_view_activity);
+		Appodeal.show(this, Appodeal.BANNER_BOTTOM);
+		url = getIntent().getStringExtra("url");
+		webView = (WebView) findViewById(R.id.webView);
+		webView.getSettings().setJavaScriptEnabled(true);
+		webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+		webView.getSettings().setDatabaseEnabled(true);
+		webView.getSettings().setDomStorageEnabled(true);
+		webView.getSettings().setBuiltInZoomControls(true);
+		webView.getSettings().setSupportZoom(true);
+		webView.getSettings().setLoadWithOverviewMode(true);
+		webView.getSettings().setUseWideViewPort(true);
+		webView.setWebViewClient(new WebViewClient() {
+			@Override
+			public void onPageStarted(WebView view, String url, Bitmap favicon) {
+				if (refreshMenuItem != null) {
+					refreshMenuItem.setActionView(R.layout.action_progressbar);
+					refreshMenuItem.expandActionView();
+				}
+				super.onPageStarted(view, url, favicon);
+			}
+
+			@Override
+			public void onPageFinished(WebView view, String url) {
+				if (refreshMenuItem != null) {
+					refreshMenuItem.collapseActionView();
+					refreshMenuItem.setActionView(null);
+				}
+				super.onPageFinished(view, url);
+			}
+		});
+		webView.setWebChromeClient(new WebChromeClient());
+		webView.getSettings()
+				.setUserAgentString(
+						"Mozilla/5.0 (Linux; Android 4.4; Nexus 4 Build/KRT16H) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/30.0.0.0 Mobile Safari/537.36");
+		// webView.loadUrl(getIntent().getStringExtra("url"));
+
+		itemsList = ((MyApplication) this.getApplication()).getItemsList();
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.sherlock_spinner_dropdown_item,
+				getItemNameList(itemsList));
+		actionBar = getSupportActionBar();
+		if (actionBar != null) {
+			actionBar.setTitle("");
+			actionBar.setDisplayUseLogoEnabled(false);
+			actionBar.setDisplayHomeAsUpEnabled(true);
+			actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+			navigationListener = new NavigationListener();
+			actionBar.setListNavigationCallbacks(adapter, navigationListener);
+			adapter.setDropDownViewResource(R.layout.sherlock_spinner_dropdown_item);
+			actionBar.setSelectedNavigationItem(getPositionByURL(url));
 		}
-		super.onPageStarted(view, url, favicon);
-	    }
-
-	    @Override
-	    public void onPageFinished(WebView view, String url) {
-		if (refreshMenuItem != null) {
-		    refreshMenuItem.collapseActionView();
-		    refreshMenuItem.setActionView(null);
-		}
-		super.onPageFinished(view, url);
-	    }
-	});
-	webView.setWebChromeClient(new WebChromeClient());
-	webView.getSettings()
-		.setUserAgentString(
-			"Mozilla/5.0 (Linux; Android 4.4; Nexus 4 Build/KRT16H) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/30.0.0.0 Mobile Safari/537.36");
-	// webView.loadUrl(getIntent().getStringExtra("url"));
-
-	itemsList = ((MyApplication) this.getApplication()).getItemsList();
-
-	ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.sherlock_spinner_dropdown_item, getItemNameList(itemsList));
-	actionBar = getSupportActionBar();
-	if (actionBar != null) {
-	    actionBar.setTitle("");
-	    actionBar.setDisplayUseLogoEnabled(false);
-	    actionBar.setDisplayHomeAsUpEnabled(true);
-	    actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
-	    navigationListener = new NavigationListener();
-	    actionBar.setListNavigationCallbacks(adapter, navigationListener);
-	    adapter.setDropDownViewResource(R.layout.sherlock_spinner_dropdown_item);
-	    actionBar.setSelectedNavigationItem(getPositionByURL(url));
 	}
-    }
 
-    private class NavigationListener implements ActionBar.OnNavigationListener {
+	private class NavigationListener implements ActionBar.OnNavigationListener {
 
-	private boolean loadUrl = true;
+		private boolean loadUrl = true;
+
+		@Override
+		public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+			if (getPositionByURL(url) == -1) {
+				actionBar.setIcon(R.drawable.logo);
+			} else {
+				url = itemsList.get(itemPosition).getUrl();
+				actionBar.setIcon(itemsList.get(itemPosition).getImageId());
+			}
+			if (loadUrl) {
+				webView.loadUrl(url);
+			} else {
+				loadUrl = true;
+			}
+
+			return false;
+		}
+
+		void doNotLoadUrl() {
+			this.loadUrl = false;
+		}
+
+	}
 
 	@Override
-	public boolean onNavigationItemSelected(int itemPosition, long itemId) {
-	    if (getPositionByURL(url) == -1) {
-		actionBar.setIcon(R.drawable.logo);
-	    } else {
-		url = itemsList.get(itemPosition).getUrl();
-		actionBar.setIcon(itemsList.get(itemPosition).getImageId());
-	    }
-	    if (loadUrl) {
-		webView.loadUrl(url);
-	    } else {
-		loadUrl = true;
-	    }
-
-	    return false;
+	public void onResume() {
+		super.onResume();
+		Appodeal.onResume(this, Appodeal.BANNER);
 	}
 
-	void doNotLoadUrl() {
-	    this.loadUrl = false;
-	}
+	@Override
+	public void onBackPressed() {
+		if (webView.canGoBack()) {
 
-    }
-
-    @Override
-    public void onResume() {
-	super.onResume();
-	Appodeal.onResume(this, Appodeal.BANNER);
-    }
-
-    @Override
-    public void onBackPressed() {
-	if (webView.canGoBack()) {
-
-	    WebBackForwardList forwardList = webView.copyBackForwardList();
-	    if (forwardList.getCurrentIndex() > 0) {
-		String previousUrl = forwardList.getItemAtIndex(forwardList.getCurrentIndex() - 1).getUrl();
-		System.out.println("!!!!!!!!!!!!!!   " + previousUrl);
-		int position = getPositionByURL(previousUrl);
-		System.out.println("!!!!!   position " + position);
-		if (position >= 0) {
-		    navigationListener.doNotLoadUrl();
-		    actionBar.setSelectedNavigationItem(position);
+			WebBackForwardList forwardList = webView.copyBackForwardList();
+			if (forwardList.getCurrentIndex() > 0) {
+				String previousUrl = forwardList.getItemAtIndex(forwardList.getCurrentIndex() - 1).getUrl();
+				System.out.println("!!!!!!!!!!!!!!   " + previousUrl);
+				int position = getPositionByURL(previousUrl);
+				System.out.println("!!!!!   position " + position);
+				if (position >= 0) {
+					navigationListener.doNotLoadUrl();
+					actionBar.setSelectedNavigationItem(position);
+				}
+			}
+			webView.goBack();
+		} else {
+			super.onBackPressed();
 		}
-	    }
-	    webView.goBack();
-	} else {
-	    super.onBackPressed();
 	}
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-	switch (item.getItemId()) {
-	case android.R.id.home:
-	    this.finish();
-	    Intent intent = new Intent(this, MainActivity.class);
-	    startActivity(intent);
-	    break;
-	case R.id.progress:
-	    webView.reload();
-	    break;
-	case R.id.menu_about:
-	    (new DialogAboutBuilder()).buildAndShowDialog(this);
-	    break;
-	case R.id.menu_settings:
-	    Intent intentSettings = new Intent(this, SettingsActivity.class);
-	    startActivity(intentSettings);
-	    break;
-	default:
-	    break;
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			this.finish();
+			Intent intent = new Intent(this, MainActivity.class);
+			startActivity(intent);
+			break;
+		case R.id.progress:
+			webView.reload();
+			break;
+		case R.id.menu_about:
+			(new DialogAboutBuilder()).buildAndShowDialog(this);
+			break;
+		case R.id.menu_settings:
+			Intent intentSettings = new Intent(this, SettingsActivity.class);
+			startActivity(intentSettings);
+			break;
+		default:
+			break;
+		}
+		return true;
 	}
-	return true;
-    }
 
-    private int getPositionByURL(String url) {
-	for (GridItem item : itemsList) {
-	    if (item.getUrl().equals(url)) {
-		return itemsList.indexOf(item);
-	    }
+	private int getPositionByURL(String url) {
+		for (GridItem item : itemsList) {
+			if (item.getUrl().equals(url)) {
+				return itemsList.indexOf(item);
+			}
+		}
+		return -1;
 	}
-	return -1;
-    }
 
-    private ArrayList<String> getItemNameList(List<GridItem> items) {
-	ArrayList<String> result = new ArrayList<String>();
-	for (GridItem item : items) {
-	    result.add(item.getName());
+	private ArrayList<String> getItemNameList(List<GridItem> items) {
+		ArrayList<String> result = new ArrayList<String>();
+		for (GridItem item : items) {
+			result.add(item.getName());
+		}
+		return result;
 	}
-	return result;
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-	getSupportMenuInflater().inflate(R.menu.web_view, menu);
-	refreshMenuItem = menu.findItem(R.id.progress);
-	return super.onCreateOptionsMenu(menu);
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getSupportMenuInflater().inflate(R.menu.web_view, menu);
+		refreshMenuItem = menu.findItem(R.id.progress);
+		return super.onCreateOptionsMenu(menu);
+	}
 }
--- src/com/rubyapps/addictedtostyle/app/MyApplication.java
+++ src/com/rubyapps/addictedtostyle/app/MyApplication.java
@@ -12,32 +12,33 @@ import com.rubyapps.addictedtostyle.model.GridItem;
 
 public class MyApplication extends Application {
 
-    private static MyApplication mInstance;
+	private static MyApplication mInstance;
 
-    private List<GridItem> items = new ArrayList<GridItem>(Arrays.asList(new GridItem(R.drawable.fashion, "Fashion",
-	    "http://www.fashionpolicenigeria.com/"),// TODO read-more
-	    new GridItem(R.drawable.beauty, "Beauty", "http://www.kamdora.com/"), new GridItem(R.drawable.hair, "Hair",
-		    "http://www.voiceofhair.com/category/hairspiration/"), new GridItem(R.drawable.wedding, "Weddings",
-		    "http://www.weddingdigestnaija.com/"), new GridItem(R.drawable.celebrity_style, "Celebrity Style",
-		    "http://blog.glamafrica.com/category/celebs/"), new GridItem(R.drawable.lifestyle_relationships, "Lifestyle: Relationship",
-		    "http://www.cosmopolitan.ng/relationships/"), new GridItem(R.drawable.diy_fashion_tips, "DIY Fashion Tips",
-		    "http://addictedtostyle.net/"), new GridItem(R.drawable.juicy_gist, "Juicy Gist", "http://yabaleftonline.com/"), new GridItem(
-		    R.drawable.giveaway, "Giveaway", "http://addictedtostyle.net/category/giveaway/"), new GridItem(R.drawable.rate_us,
-		    "Rate & Update", " https://play.google.com/store/apps/details?id=com.rubyapps.addictedtostyle")));
+	private List<GridItem> items = new ArrayList<GridItem>(Arrays.asList(new GridItem(R.drawable.fashion, "Fashion",
+			"http://www.fashionpolicenigeria.com/"), new GridItem(R.drawable.beauty, "Beauty",
+			"http://www.kamdora.com/"), new GridItem(R.drawable.hair, "Hair",
+			"http://www.voiceofhair.com/category/hairspiration/"), new GridItem(R.drawable.wedding, "Weddings",
+			"http://www.weddingdigestnaija.com/"), new GridItem(R.drawable.celebrity_style, "Celebrity Style",
+			"http://blog.glamafrica.com/category/celebs/"), new GridItem(R.drawable.lifestyle_relationships,
+			"Lifestyle: Relationship", "http://www.cosmopolitan.ng/relationships/"), new GridItem(
+			R.drawable.diy_fashion_tips, "DIY Fashion Tips", "http://addictedtostyle.net/"), new GridItem(
+			R.drawable.juicy_gist, "Juicy Gist", "http://yabaleftonline.com/"), new GridItem(R.drawable.giveaway,
+			"Giveaway", "http://addictedtostyle.net/category/giveaway/"), new GridItem(R.drawable.rate_us,
+			"Rate & Update", " https://play.google.com/store/apps/details?id=com.rubyapps.addictedtostyle")));
 
-    public List<GridItem> getItemsList() {
-	return items;
-    }
+	public List<GridItem> getItemsList() {
+		return items;
+	}
 
-    @Override
-    public void onCreate() {
-	super.onCreate();
-	mInstance = this;
-	// register with parse
-	ParseUtils.registerParse(this);
-    }
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		mInstance = this;
+		// register with parse
+		ParseUtils.registerParse(this);
+	}
 
-    public static synchronized MyApplication getInstance() {
-	return mInstance;
-    }
+	public static synchronized MyApplication getInstance() {
+		return mInstance;
+	}
 }
